5d053d86fee28774d0eff20ba2eb26574614013d
Yueh Han <yuehwhan@gmail.com>
2019-12-15 20:06:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,13 +16,13 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+    win_index_0 = win_combo[0]
+    win_index_1 = win_combo[1]
+    win_index_2 = win_combo[2]
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
+    position_1 = board[win_index_0]
+    position_2 = board[win_index_1]
+    position_3 = board[win_index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
@@ -34,7 +34,7 @@ def won?(board)
 end
 
 def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
+  board.all?
 end
 
 def draw?(board)
